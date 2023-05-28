87d451cedd2b21046770dedb51bb2a452b2b0a09
Labbi-Ahmed <labbi5689@gmail.com>
2023-05-28 15:25:07 +0600

    initial commit for owner of the house projects

---
+++ src/main/java/com/labbi/rentyourhome/RentYourHomeApplication.java
@@ -0,0 +1,13 @@
+package com.labbi.rentyourhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentYourHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentYourHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labbi/rentyourhome/RentYourHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labbi.rentyourhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentYourHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
